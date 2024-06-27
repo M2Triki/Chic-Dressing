b9dcc2e9fbfe61e8635be1421160d0979980f2d1
Matheo TRIKI <matheo.triki@gmail.com>
2024-06-27 17:58:46 +0200

    Commit après appel mentor

--- app/public/wp-content/themes/chicdressing/functions.php
+++ app/public/wp-content/themes/chicdressing/functions.php
@@ -10,15 +10,11 @@ add_filter( 'big_image_size_threshold', '__return_false' );
 
 function remove_ashe_google_fonts() {
     wp_dequeue_style('ashe-playfair-font');
-    wp_deregister_style('ashe-playfair-font');
 
     wp_dequeue_style('ashe-opensans-font');
-    wp_deregister_style('ashe-opensans-font');
 
     wp_dequeue_style('ashe-kalam-font');
-    wp_deregister_style('ashe-kalam-font');
 
     wp_dequeue_style('ashe-rokkitt-font');
-    wp_deregister_style('ashe-rokkitt-font');
 }
 add_action('wp_enqueue_scripts', 'remove_ashe_google_fonts', 999);
